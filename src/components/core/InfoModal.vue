f210f69e29c192335643fd32943621015ab8c57e
josuermartinezm <78878134+josuermartinezm@users.noreply.github.com>
2022-01-14 18:55:57 -0400

    Fixes

--- src/components/core/InfoModal.vue
+++ src/components/core/InfoModal.vue
@@ -2,14 +2,14 @@
     <div class="min-w-screen max-h-screen  overflow-auto animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
          :style="{background: 'rgb(122,122,122, 0.5)'}" id="modal-id" v-show="visible">
         <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
-        <div class="w-3/4  w-16 p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white flex">
+        <div class="w-1/2 p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white flex">
             <button class="absolute inset-x-0top-0 right-0 px-5"  @click="close">
                 <span class="text-secondary material-icons">close</span>
             </button>
             <div class="max-h-full">
                 <div class="text-center p-5 flex-auto justify-center">
                     <h2 class="text-3xl font-bold py-2 leading-none text-primary">{{title}}</h2>
-                    <div class="p-6 w-full grid grid-cols-12 gap-4 mb-5 text-justify">
+                    <div class="p-6 inner-content w-full grid grid-cols-12 gap-4 mb-5 text-justify">
                         <div class="col-span-12" v-html="contentText"></div>
                     </div>
 
@@ -53,5 +53,10 @@
         width: 1em; /* Also needed for space (tweak if needed) */
         margin-left: -1em; /* Also needed for space (tweak if needed) */
         font-size: 24px;
+    }
+
+    .inner-content {
+        max-height: 70vh;
+        overflow-y: auto;
     }
 </style>
